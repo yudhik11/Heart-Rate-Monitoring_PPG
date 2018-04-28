04311473368165e8fb57fcd772ddc70fc72fff29
yudhik11 <yudhik100@gmail.com>
2018-04-28 23:57:48 +0530

    script updated

--- final/script.m
+++ final/script.m
@@ -1,25 +1,43 @@
-tic
-load ../Project/Data/DATA_03_TYPE02.mat
-L = length(sig);
-pl = -1, pBPM = -1, ptc = 0, plssr = 0, HR = [];
-for i=5:-1:1%floor((x-1000)/250+1)
-    x = sig(:,(i-1)*250+1:((i-1)*250) + 1000);
-    y = x;
-    [cl, cBPM, tr, lssr] = SMART(y, pl, pBPM, ptc, 1000, 125, plssr)
-    pl = cl;
-    pBPM = cBPM;
-    ptc = tr;
-    plssr = lssr;
+function [] = script(training_path, testing_path, saving_path)
+    clc
+    clear all
+    close all
+
+    tic
+    Files = dir(testing_path)
+    counter = 1;
+    for k=1:length(Files)
+        FileNames=Files(k).name
+        if FileNames == '.'
+            continue
+        elseif Filenames == '..'
+            continue
+        end
+        str = strcat(testing_path, FileNames)
+        load str
+        L = length(sig);
+        pl = -1, pBPM = -1, ptc = 0, plssr = 0, HR = [];
+        for i=20:-1:1%floor((x-1000)/250+1)
+            x = sig(:,(i-1)*250+1:((i-1)*250) + 1000);
+            y = x;
+            [cl, cBPM, tr, lssr] = SMART(y, pl, pBPM, ptc, 1000, 125, plssr)
+            pl = cl;
+            pBPM = cBPM;
+            ptc = tr;
+            plssr = lssr;
+        end
+        for i=1:125%floor((x-1000)/250+1)
+            x = sig(:,(i-1)*250+1:((i-1)*250) + 1000);
+            y = x;
+            [cl, cBPM, tr, lssr] = SMART(y, pl, pBPM, ptc, 1000, 125, plssr);
+            pl = cl;
+            pBPM = cBPM;
+            ptc = tr;
+            plssr = lssr;
+            HR = [HR; cBPM];
+        end
+        HR
+        save(saving_path+"output_team_42_"+FileNames, HR)
+    end
+    toc
 end
-for i=1:5%floor((x-1000)/250+1)
-    x = sig(:,(i-1)*250+1:((i-1)*250) + 1000);
-    y = x;
-    [cl, cBPM, tr, lssr] = SMART(y, pl, pBPM, ptc, 1000, 125, plssr);
-    pl = cl;
-    pBPM = cBPM;
-    ptc = tr;
-    plssr = lssr;
-    HR = [HR cBPM];
-end
-HR
-toc
