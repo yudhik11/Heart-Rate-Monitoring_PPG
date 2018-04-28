aa7eb8d9ba05b760c1fdc87e1d0dc7f7f8d69ade
yudhik11 <yudhik100@gmail.com>
2018-04-28 21:20:36 +0530

    iteration limit changed

--- final/script.m
+++ final/script.m
@@ -11,7 +11,7 @@ for i=20:-1:1%floor((x-1000)/250+1)
     ptc = tr;
     plssr = lssr;
 end
-for i=1:120%floor((x-1000)/250+1)
+for i=1:125%floor((x-1000)/250+1)
     x = sig(:,(i-1)*250+1:((i-1)*250) + 1000);
     y = x;
     [cl, cBPM, tr, lssr] = SMART(y, pl, pBPM, ptc, 1000, 125, plssr);
