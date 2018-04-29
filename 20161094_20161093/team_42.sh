57e43bfae146c322b0aaafc580226f6cc68bccfd
yudhik11 <yudhik100@gmail.com>
2018-04-29 12:29:52 +0530

    script updated

--- 20161094_20161093/team_42.sh
+++ 20161094_20161093/team_42.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "Please variables 1 and 2:"
+echo "Training Set:"
 read var1 
+echo "Testing Set:"
 read var2
+echo "Output Directory:"
 read var3
 echo "var1=$var1, var2=$var2, var3=$var3"
 /home/$USER/matlab/bin/matlab -nodesktop -nosplash -nojvm -r "script('$var1','$var2','$var3')"
